d7d0f53c3de035617e9494185c2a4cb31cbf19ac
孙金瑶 <1323969103@qq.com>
2024-10-18 16:18:52 +0800

    create new vue2

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <header></header>
+    <footer></footer>
+  </div>
+</template>
+
+<script>
+import header from './components/header/index.vue'
+import footer from './components/footer/index.vue'
+
+export default {
+  name: 'App',
+  components: {
+    header,
+    footer,
+  }
+}
+</script>
+
+<style></style>
---
+++ src/components/footer/index.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <div class="footer">footer</div>
+
+    </div>
+</template>
+<script>
+export default {
+    name: "footer",
+
+}
+</script>
+<style scoped>
+#footer {
+    width: 100%;
+    height: 200px;
+    background-color: aqua;
+}
+</style>
